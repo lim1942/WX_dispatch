2ad4cb6a754bcf48e17cbcb3d21ec519cffedb8c
Lim <31461860+lim1942@users.noreply.github.com>
2018-04-13 09:40:32 +0800

    Update clear_redis.py

--- WX_master/clear_redis.py
+++ WX_master/clear_redis.py
@@ -3,33 +3,30 @@
 # @Email: 940711277@qq.com
 # @Date:  2018-04-04 10:06:34
 # @Last Modified by:  lim
-# @Last Modified time:  2018-04-11 11:40:16
+# @Last Modified time:  2018-04-12 10:39:09
 import time
-from tools import get_logger
 from dbs.redis_db import redis_task
 from config import LOOP_INTERVAL, DOING_CLEAR
 
 CURSOR = redis_task()
-clear_redis_log = get_logger('clear_redis')
 
 
 def clear_redis():
     """func for trim redis finish & failed set."""
     record = 0
     tatoal = DOING_CLEAR*86400
+
     while True:
         time.sleep(LOOP_INTERVAL)
-        try:
-            record +=LOOP_INTERVAL
-            CURSOR.trim_finish_set()
-            CURSOR.trim_failed_set()
-            if record >= tatoal:
-                CURSOR.handle_bad_doing()
-                record = 0
-        except Exception as e:
-            clear_redis_log.error('000:clear redis error{}'.format(e.message))
-        else:
-            clear_redis_log.info('success clear redis db')
+
+        CURSOR.trim_finish_set()
+        CURSOR.trim_failed_set()
+
+        record +=LOOP_INTERVAL
+        if record >= tatoal:
+            CURSOR.handle_bad_doing()
+            record = 0
+
 
 if __name__ == '__main__':
     clear_redis()
